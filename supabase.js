ccbae83916a3302d2b6fd83ca154c4d7550030a2
zlatan2929 <ivanmggs2926@gmail.com>
2025-07-13 19:35:11 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const { createClient } = supabase;
+const supabaseUrl = 'https://VOTRE_URL.supabase.co';
+const supabaseKey = 'VOTRE_CLE_ANON';
+const supabase = createClient(supabaseUrl, supabaseKey);
+```
